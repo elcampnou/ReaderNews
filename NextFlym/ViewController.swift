7466b996b14c4b5220372cf5c840a748c5db9ee4
Elfassi Mounir <elfassimounir@imac-mounir.home>
2016-01-02 17:57:47 +0000

    Initial Commit

---
+++ NextFlym/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextFlym
+//
+//  Created by Elfassi Mounir on 1/2/16.
+//  Copyright © 2016 Elfassi Mounir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
