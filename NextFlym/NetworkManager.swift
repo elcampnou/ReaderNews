7b07b393f213b218a7b37fd16557272f114f11ce
Elfassi Mounir <elfassimounir@iMac-Mounir.Home>
2016-01-03 08:59:45 +0000

    F1  commit

--- NextFlym/NetworkManager.swift
+++ NextFlym/NetworkManager.swift
@@ -38,35 +38,93 @@ class NetworkManager: NSObject {
         let request = NSURLRequest(URL: NSURL(string: kGoogleNewsRSSURL)!)
         
         NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
-
-            
-    
         
-    
-    
-    
+            if let parsedResponse = response as? NSHTTPURLResponse {
+            
+                if (data != nil) && (error == nil) && (parsedResponse.statusCode == kHTTPResponseStatusCodeSuccess) {
+                    // good data, no error, good response status code
+                    completion(data: data, error: nil)
+                } else {
+                    if error != nil {
+                        // check error returned first
+                        if let errorCode = error?.code {
+                            switch errorCode {
+                            case NSURLErrorNotConnectedToInternet:
+                                completion(data: nil, error: NetworkError.NetworkFailure)
+                                
+                            default:
+                                completion(data: nil, error: NetworkError.UnknownError)
+                            }
+                        }
+                    } else {
+                        // non-success status codes and catch all
+                        completion(data: nil, error: NetworkError.UnknownError)
+                    }
+                }
+            }
+        }.resume()
     }
     
+    // This method takes in an NSData object and runs it through my parser class to parse the XML
+    // It returns an array of ArticlePrototype structs (the prototype object used to create my Article NSManagedObject, and/or a ParsingError
+    func parseAllArticleData(data: NSData, completion: (content: [ArticlePrototype]?, error: ErrorType?) -> Void) {
     
+        let articleParser = ArticleParser(data: data)
+        
+        articleParser.parseDataWithCompletion { (success) -> Void in
+            if success {
+                
+                // try core data
+                CoreDataManager().cacheFetchedArticles(articleParser.articles)
+                
+                completion(content: articleParser.articles, error: nil)
+            } else {
+                completion(content: nil, error: NetworkError.ParsingError)
+            }
+        }
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // This method takes in an array of ArticlePrototype objects, fetches the image for each object, and retuns are new array of ArticlePrototype objects with image properties
+    func downloadImagesForArticles(articles: [ArticlePrototype], completion: (articles: [ArticlePrototype]) -> Void) {
+        
+        if articles.isEmpty {
+            completion(articles: articles)
+        }
+        
+        var articlesWithImages = [ArticlePrototype]()
+        
+        var index = 0
+        
+        for var article in articles {
+            self.fetchImageFromURL(article.imageURL, completion: { (image) -> Void in
+                article.image = image
+                
+                articlesWithImages.append(article)
+                
+                index++
+                
+                if index == articles.count {
+                    completion(articles: articlesWithImages)
+                }
+                
+            })
+        }
+    }
+
+    // This method is used internally by downloadImagesForArticles
+    // It performs the web request to fetch the data associated with an image URL, and return a UIIimage
+    func fetchImageFromURL(url: String, completion:(image: UIImage?) -> Void) {
+        let request = NSURLRequest(URL: NSURL(string: url)!)
+        
+        NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if data != nil {
+                
+                if let unwrappedData = data as NSData! {
+                    completion(image: UIImage(data: unwrappedData))
+                }
+            } else {
+                completion(image: nil)
+            }
+            }.resume()
+    }
 }
-
-
-
-
-
-
